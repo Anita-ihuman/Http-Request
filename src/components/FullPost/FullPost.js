20910b38a1bc29ad552cfaef259bdfeb29ead1af
Anita-ihuman <charlesanita403@gmail.com>
2020-11-07 09:56:27 +0100

    A wrap up of this project

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,57 +1,51 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
-import "./FullPost.css";
+import './FullPost.css';
 
 class FullPost extends Component {
-  state = {
-    loadedPost: null,
-  };
-
-  componentDidUpdate() {
-    if (this.props.id) {
-      if (
-        !this.state.loadedPost ||
-        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
-      ) {
-        axios
-          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-          .then((response) => {
-            // console.log(response);
-            this.setState({ loadedPost: response.data });
-          });
-      }
+    state = {
+        loadedPost: null
     }
-  }
 
-  deletePostHandler = () => {
-    axios
-      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-      .then((response) => {
-        console.log(response);
-      });
-  };
+    componentDidUpdate () {
+        if ( this.props.id ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+                axios.get( '/posts/' + this.props.id )
+                    .then( response => {
+                        // console.log(response);
+                        this.setState( { loadedPost: response.data } );
+                    } );
+            }
+        }
+    }
 
-  render() {
-    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-    if (this.props.id) {
-      post = <p style={{ textAlign: "center" }}>Loading...!</p>;
+    deletePostHandler = () => {
+        axios.delete('/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
     }
-    if (this.state.loadedPost) {
-      post = (
-        <div className="FullPost">
-          <h1>{this.state.loadedPost.title}</h1>
-          <p>{this.state.loadedPost.body}</p>
-          <div className="Edit">
-            <button onClick={this.deletePostHandler} className="Delete">
-              Delete
-            </button>
-          </div>
-        </div>
-      );
+
+    render () {
+        let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+        if ( this.props.id ) {
+            post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
+        }
+        if ( this.state.loadedPost ) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+        return post;
     }
-    return post;
-  }
 }
 
 export default FullPost;
